988dcadd5a00ce5718dbb0275b6fa90fe8e73c65
TimosPal <timospale@gmail.com>
2021-03-31 19:48:17 +0300

    removed constexpr from sliding pieces

---
+++ src/MoveGeneration/SlidingPieces.cpp
@@ -0,0 +1,148 @@
+#include "SlidingPieces.h"
+
+namespace ChessEngine::MoveGeneration::SlidingPieces {
+
+    using namespace BitboardUtil;
+    using namespace MagicNumbers;
+
+    // NOTE: performance isn't crucial for these functions
+    // since they are run once when producing the tables.
+
+    /*******************************************************/
+    /* General                                             */
+    /*******************************************************/
+
+    /* Generate a move table based on the given function for each board position. */
+    std::array<Bitboard, 64> InitMasksTable(Bitboard getMask(uint8_t, uint8_t)) {
+        std::array<Bitboard, 64> mask = {};
+
+        for (uint8_t rank = 0; rank < 8; rank++) {
+            for (uint8_t file = 0; file < 8; file++) {
+                uint8_t squareIndex = GetSquareIndex(file, rank);
+                mask[squareIndex] = getMask((File)file, (Rank)rank);
+            }
+        }
+
+        return mask;
+    }
+
+    /* Generate a mask based on an initial position and direction */
+    Bitboard GetDirectionalBlockerMask(uint8_t file, uint8_t rank, int8_t dirX, int8_t dirY){
+        // Exclude outer edges.
+        Bitboard mask = BITBOARD_EMPTY;
+        int x = file + dirX, y = rank + dirY;
+        while(x > 0 && x < 7 && y > 0 && y < 7) {
+            mask |= SetBit(BITBOARD_EMPTY, GetSquareIndex(x, y));
+            x += dirX;
+            y += dirY;
+        }
+
+        return mask;
+    }
+
+    /* Generate an attack mask based on an initial position , direction and occupancies */
+    Bitboard GetDirectionalMoves(uint8_t file, uint8_t rank, int8_t dirX, int8_t dirY, Bitboard occupancies){
+        // Include outer edges if not blocked till that point.
+        Bitboard mask = BITBOARD_EMPTY;
+        int x = file + dirX, y = rank + dirY;
+        while(x >= 0 && x <= 7 && y >= 0 && y <= 7) {
+            Bitboard currentBit = SetBit(BITBOARD_EMPTY, GetSquareIndex(x, y));
+            mask |= currentBit;
+
+            // We found a piece and we should stop.
+            if((currentBit & occupancies) != 0)
+                break;
+
+            x += dirX;
+            y += dirY;
+        }
+
+        return mask;
+    }
+
+    /*******************************************************/
+    /* Rook                                                */
+    /*******************************************************/
+
+    Bitboard GetRookBlockerMask(uint8_t file, uint8_t rank){
+        Bitboard mask = BITBOARD_EMPTY;
+
+        mask |= GetDirectionalBlockerMask(file, rank, 0, 1);
+        mask |= GetDirectionalBlockerMask(file, rank, 0, -1);
+        mask |= GetDirectionalBlockerMask(file, rank, 1, 0);
+        mask |= GetDirectionalBlockerMask(file, rank, -1, 0);
+
+        return mask;
+    }
+
+    Bitboard GetRookMoves(uint8_t file, uint8_t rank, Bitboard occupancies){
+        Bitboard mask = BITBOARD_EMPTY;
+
+        mask |= GetDirectionalMoves(file, rank, 0, 1, occupancies);
+        mask |= GetDirectionalMoves(file, rank, 0, -1, occupancies);
+        mask |= GetDirectionalMoves(file, rank, 1, 0, occupancies);
+        mask |= GetDirectionalMoves(file, rank, -1, 0, occupancies);
+
+        // Exclude self.
+        return mask;
+    }
+
+    /*******************************************************/
+    /* Bishop                                              */
+    /*******************************************************/
+
+    Bitboard GetBishopBlockerMask(uint8_t file, uint8_t rank){
+        Bitboard mask = BITBOARD_EMPTY;
+
+        mask |= GetDirectionalBlockerMask(file, rank, 1, 1);
+        mask |= GetDirectionalBlockerMask(file, rank, 1, -1);
+        mask |= GetDirectionalBlockerMask(file, rank, -1, 1);
+        mask |= GetDirectionalBlockerMask(file, rank, -1, -1);
+
+        return mask;
+    }
+
+    Bitboard GetBishopMoves(uint8_t file, uint8_t rank, Bitboard occupancies){
+        Bitboard mask = BITBOARD_EMPTY;
+
+        mask |= GetDirectionalMoves(file, rank, 1, 1, occupancies);
+        mask |= GetDirectionalMoves(file, rank, 1, -1, occupancies);
+        mask |= GetDirectionalMoves(file, rank, -1, 1, occupancies);
+        mask |= GetDirectionalMoves(file, rank, -1, -1, occupancies);
+
+        // Exclude self.
+        return mask;
+    }
+
+    void InitMovesForSquare(std::array<Bitboard, permutations>& slidingMoves, uint8_t squareIndex, bool forBishop){
+        auto [file, rank] = GetCoordinates(squareIndex);
+        Bitboard blockerMask = forBishop ? GetBishopBlockerMask(file, rank) :
+                               GetRookBlockerMask(file, rank);
+        uint8_t bitCount = GetBitCount(blockerMask);
+
+        for (uint16_t permutationIndex = 0; permutationIndex < (1 << bitCount); permutationIndex++) {
+            Bitboard occupanciesPermutation = GetPermutation(blockerMask, permutationIndex);
+            uint64_t index = forBishop ? BishopMagicHash(blockerMask, squareIndex, bitCount) :
+                             RookMagicHash(blockerMask, squareIndex, bitCount);
+            Bitboard attacks = GetBishopMoves(file, rank, occupanciesPermutation);
+
+            slidingMoves[index] = attacks;
+        }
+    }
+
+
+    /*******************************************************/
+    /* Attack masks                                        */
+    /*******************************************************/
+
+    // Blocker masks , to be used in magic bitboards.
+    const std::array<Bitboard, 64> rookMasks = InitMasksTable(GetRookBlockerMask);
+    const std::array<Bitboard, 64> bishopMasks = InitMasksTable(GetBishopBlockerMask);
+
+    // Used instead of GetBitCount in run time calculations.
+    const std::array<uint64_t , 64> rookMaskBitCounts =
+            InitMasksTable([](File file, Rank rank) { return (uint64_t)GetBitCount(rookMasks[GetSquareIndex(file, rank)]); } );
+    const std::array<uint64_t , 64> bishopMaskBitCounts =
+            InitMasksTable([](File file, Rank rank) { return (uint64_t)GetBitCount(bishopMasks[GetSquareIndex(file, rank)]); } );
+
+}
--- src/MoveGeneration/SlidingPieces.h
+++ src/MoveGeneration/SlidingPieces.h
@@ -1,153 +1,54 @@
 #ifndef SLIDINGPIECES_H
 #define SLIDINGPIECES_H
 
-#include "MagicNumbers.h"
 #include "../Board/Bitboard.h"
+#include "MagicNumbers.h"
 
-namespace ChessEngine::SlidingPieces {
-
-    using namespace BitboardUtil;
-    using namespace MagicNumbers;
-
-    // NOTE: performance isn't crucial for these functions
-    // since they are run once when producing the tables.
+namespace ChessEngine::MoveGeneration::SlidingPieces {
 
     /*******************************************************/
     /* General                                             */
     /*******************************************************/
 
     /* Generate a move table based on the given function for each board position. */
-    constexpr std::array<Bitboard, 64> InitMasksTable(Bitboard getMask(File, Rank)) {
-        std::array<Bitboard, 64> mask = {};
-
-        for (uint8_t rank = 0; rank < 8; rank++) {
-            for (uint8_t file = 0; file < 8; file++) {
-                uint8_t squareIndex = GetSquareIndex(file, rank);
-                mask[squareIndex] = getMask((File)file, (Rank)rank);
-            }
-        }
-
-        return mask;
-    }
+    std::array<BitboardUtil::Bitboard, 64> InitMasksTable(BitboardUtil::Bitboard getMask(uint8_t, uint8_t));
 
     /* Generate a mask based on an initial position and direction */
-    constexpr Bitboard GetDirectionalBlockerMask(File file, Rank rank, int8_t dirX, int8_t dirY){
-        // Exclude outer edges.
-        Bitboard mask = BITBOARD_EMPTY;
-        int x = file + dirX, y = rank + dirY;
-        while(x > 0 && x < 7 && y > 0 && y < 7) {
-            mask |= SetBit(BITBOARD_EMPTY, GetSquareIndex(x, y));
-            x += dirX;
-            y += dirY;
-        }
-
-        return mask;
-    }
+    BitboardUtil::Bitboard GetDirectionalBlockerMask(uint8_t file, uint8_t rank, int8_t dirX, int8_t dirY);
 
     /* Generate an attack mask based on an initial position , direction and occupancies */
-    constexpr Bitboard GetDirectionalMoves(File file, Rank rank, int8_t dirX, int8_t dirY, Bitboard occupancies){
-        // Include outer edges if not blocked till that point.
-        Bitboard mask = BITBOARD_EMPTY;
-        int x = file + dirX, y = rank + dirY;
-        while(x >= 0 && x <= 7 && y >= 0 && y <= 7) {
-            Bitboard currentBit = SetBit(BITBOARD_EMPTY, GetSquareIndex(x, y));
-            mask |= currentBit;
-
-            // We found a piece and we should stop.
-            if((currentBit & occupancies) != 0)
-                break;
-
-            x += dirX;
-            y += dirY;
-        }
-
-        return mask;
-    }
+    BitboardUtil::Bitboard GetDirectionalMoves(uint8_t file, uint8_t rank, int8_t dirX, int8_t dirY, BitboardUtil::Bitboard occupancies);
 
     /*******************************************************/
     /* Rook                                                */
     /*******************************************************/
 
-    constexpr Bitboard GetRookBlockerMask(File file, Rank rank){
-        Bitboard mask = BITBOARD_EMPTY;
+    BitboardUtil::Bitboard GetRookBlockerMask(uint8_t file, uint8_t rank);
 
-        mask |= GetDirectionalBlockerMask(file, rank, 0, 1);
-        mask |= GetDirectionalBlockerMask(file, rank, 0, -1);
-        mask |= GetDirectionalBlockerMask(file, rank, 1, 0);
-        mask |= GetDirectionalBlockerMask(file, rank, -1, 0);
-
-        return mask;
-    }
-
-    constexpr Bitboard GetRookMoves(File file, Rank rank, Bitboard occupancies){
-        Bitboard mask = BITBOARD_EMPTY;
-
-        mask |= GetDirectionalMoves(file, rank, 0, 1, occupancies);
-        mask |= GetDirectionalMoves(file, rank, 0, -1, occupancies);
-        mask |= GetDirectionalMoves(file, rank, 1, 0, occupancies);
-        mask |= GetDirectionalMoves(file, rank, -1, 0, occupancies);
-
-        // Exclude self.
-        return mask;
-    }
+    BitboardUtil::Bitboard GetRookMoves(uint8_t file, uint8_t rank, BitboardUtil::Bitboard occupancies);
 
     /*******************************************************/
     /* Bishop                                              */
     /*******************************************************/
 
-    constexpr Bitboard GetBishopBlockerMask(File file, Rank rank){
-        Bitboard mask = BITBOARD_EMPTY;
+    BitboardUtil::Bitboard GetBishopBlockerMask(uint8_t file, uint8_t rank);
 
-        mask |= GetDirectionalBlockerMask(file, rank, 1, 1);
-        mask |= GetDirectionalBlockerMask(file, rank, 1, -1);
-        mask |= GetDirectionalBlockerMask(file, rank, -1, 1);
-        mask |= GetDirectionalBlockerMask(file, rank, -1, -1);
+    BitboardUtil::Bitboard GetBishopMoves(uint8_t file, uint8_t rank, BitboardUtil::Bitboard occupancies);
 
-        return mask;
-    }
-
-    constexpr Bitboard GetBishopMoves(File file, Rank rank, Bitboard occupancies){
-        Bitboard mask = BITBOARD_EMPTY;
-
-        mask |= GetDirectionalMoves(file, rank, 1, 1, occupancies);
-        mask |= GetDirectionalMoves(file, rank, 1, -1, occupancies);
-        mask |= GetDirectionalMoves(file, rank, -1, 1, occupancies);
-        mask |= GetDirectionalMoves(file, rank, -1, -1, occupancies);
-
-        // Exclude self.
-        return mask;
-    }
-
-    constexpr void InitMovesForSquare(std::array<Bitboard, permutations>& slidingMoves, uint8_t squareIndex, bool forBishop){
-        auto [file, rank] = GetCoordinates(squareIndex);
-        Bitboard blockerMask = forBishop ? GetBishopBlockerMask(file, rank) :
-                                           GetRookBlockerMask(file, rank);
-        uint8_t bitCount = GetBitCount(blockerMask);
-
-        for (uint16_t permutationIndex = 0; permutationIndex < (1 << bitCount); permutationIndex++) {
-            Bitboard occupanciesPermutation = GetPermutation(blockerMask, permutationIndex);
-            uint64_t index = forBishop ? BishopMagicHash(blockerMask, squareIndex, bitCount) :
-                                         RookMagicHash(blockerMask, squareIndex, bitCount);
-            Bitboard attacks = GetBishopMoves(file, rank, occupanciesPermutation);
-
-            slidingMoves[index] = attacks;
-        }
-    }
-
+    void InitMovesForSquare(std::array<BitboardUtil::Bitboard, MagicNumbers::permutations>& slidingMoves, uint8_t squareIndex, bool forBishop);
 
     /*******************************************************/
     /* Attack masks                                        */
     /*******************************************************/
 
     // Blocker masks , to be used in magic bitboards.
-    constexpr std::array<Bitboard, 64> rookMasks = InitMasksTable(GetRookBlockerMask);
-    constexpr std::array<Bitboard, 64> bishopMasks = InitMasksTable(GetBishopBlockerMask);
+    extern const std::array<BitboardUtil::Bitboard, 64> rookMasks;
+    extern const std::array<BitboardUtil::Bitboard, 64> bishopMasks;
 
     // Used instead of GetBitCount in run time calculations.
-    constexpr std::array<uint64_t , 64> rookMaskBitCounts =
-            InitMasksTable([](File file, Rank rank) { return (uint64_t)GetBitCount(rookMasks[GetSquareIndex(file, rank)]); } );
-    constexpr std::array<uint64_t , 64> bishopMaskBitCounts =
-            InitMasksTable([](File file, Rank rank) { return (uint64_t)GetBitCount(bishopMasks[GetSquareIndex(file, rank)]); } );
+    extern const std::array<uint64_t , 64> rookMaskBitCounts;
+    extern const std::array<uint64_t , 64> bishopMaskBitCounts;
+
 
 }
 
